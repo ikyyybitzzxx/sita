89991801fb8c449e278361343d169a0c38b884ac
ikyyybitzzxx <106673608+ikyyybitzzxx@users.noreply.github.com>
2022-08-17 09:03:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,31 +5,31 @@
 // RECODE BY OSCAR OFC
 
 //=============『 Utama 』================== //
-global.owner = ['6285850539404','79609055704'] 
+global.owner = ['6285922480867','6289510033986'] 
 global.mods = [''] 
-global.prems = ['6285850539404'] 
+global.prems = ['6285922480867'] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ᴹᴿ᭄ Jarot offcঔৣ꧂'
-global.numberowner = '6285850539404','79609055704',
-global.instagram = 'https://instagram.com/Jarotr_'
-global.github = 'https://github.com/Alpiii22'
-global.Youtube = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
-global.sc = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
-global.ss = 'https://youtu.be/3m7tRD4RklM'
-global.sss = 'https://youtu.be/PzUH1ApMCig'
+global.nameowner = 'Kyy'
+global.numberowner = '6285922480867','6289510033986',
+global.instagram = 'https://github.com/ikyyybitzzxx'
+global.github = 'https://github.com/ikyyybitzzxx'
+global.Youtube = 'https://youtube.com/channel/UCFDu3WbiZbKX51d0GGDJWpA'
+global.sc = 'https://youtube.com/xnxx.com'
+global.ss = 'https://youtube.com/channel/UCFDu3WbiZbKX51d0GGDJWpA'
+global.sss = 'https://youtube.com/channel/UCFDu3WbiZbKX51d0GGDJWpA'
 global.yt = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
-global.dana = '085850539404'
-global.pulsa = '085850539404'
-global.gopay = '085850539404'
+global.dana = '085922480867'
+global.pulsa = '085922480867'
+global.gopay = '085922480867'
 
 //=============『 Info Bot 』=================//
-global.namebot = '❋ཻུ۪۪⸙Darkbotz-MD⳹ ❋ཻུ۪۪'
-global.gc = 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+global.namebot = 'BotYanz'
+global.gc = 'https://chat.whatsapp.com/F7imKOvW83K3uxKKYWN6op'
 global.web = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.webm = 'https://instagram.com/Jarotr_'
-global.pulsa = '085850539404'
-global.nomer = 'https://wa.me/6285850539404' //ganti jadi nomor mu
+global.webm = 'https://instagram.com/this.seira'
+global.pulsa = '085922480867'
+global.nomer = 'https://wa.me/6285922480867' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERBULAN Rp 15.000\nMAKSIMAL 2 GRUP\n》2 BULAN 25.000\nMAKSIMAL 3 GRUP \n》3 BULAN Rp 40.000\nMAKSIMAL 2 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
